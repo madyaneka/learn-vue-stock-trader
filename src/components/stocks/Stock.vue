645c63c8a753da62f889f388e82b938569e60497
Madyan Eka <madyan.eka95@gmail.com>
2018-10-20 21:27:58 +0700

    logging stock with buy button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,18 +2,49 @@
   <div class="column is-half">
     <nav class="panel">
       <p class="panel-heading has-background-info">
-        NAME <small class="is-size-7">(Price: PRICE)</small>
+        {{ stock.name }} <small class="is-size-7">(Price: {{ stock.price }})</small>
       </p>
       <div class="panel-block">
         <div class="field has-addons">
           <p class="control">
-            <input type="number" class="input" placeholder="Quantity">
+            <input
+              type="number"
+              class="input"
+              placeholder="Quantity"
+              v-model.number="quantity">
           </p>
           <p class="control">
-            <button class="button is-info">Buy</button>
+            <button
+              class="button is-info"
+              @click="buyStock"
+              :disabled="(quantity < 1) || (!Number.isInteger(quantity))">Buy</button>
           </p>
         </div>
       </div>
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['stock'],
+
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order)
+      this.quantity = 0
+    }
+  }
+}
+</script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="columns is-multiline">
-    <app-stock v-for="stock in stocks" :key="stock.id"></app-stock>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
   </div>
 </template>
 
