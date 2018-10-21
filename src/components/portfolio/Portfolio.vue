bb016bce8b1d366b813d009b156565a03aa92daa
Madyan Eka <madyan.eka95@gmail.com>
2018-10-21 07:11:10 +0700

    connecting the portfolio with vuex

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>The Portfolio Component</h1>
+  <div class="columns is-multiline">
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Stock from './Stock'
+
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: 'stockPortfolio'
+    })
+  },
+
+  components: {
+    appStock: Stock
+  }
+}
+</script>
--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,54 @@
 <template>
-  <h1>The Portfolio - Stock Component</h1>
+  <div class="column is-half">
+    <nav class="panel">
+      <p class="panel-heading has-background-info">
+        {{ stock.name }} <small class="is-size-7">(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+      </p>
+      <div class="panel-block">
+        <div class="field has-addons">
+          <p class="control">
+            <input
+              type="number"
+              class="input"
+              placeholder="Quantity"
+              v-model.number="quantity">
+          </p>
+          <p class="control">
+            <button
+              class="button is-info"
+              @click="sellStock"
+              :disabled="(quantity < 1) || (!Number.isInteger(quantity))">Sell</button>
+          </p>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: ['stock'],
+
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+
+  methods: {
+    ...mapActions([
+      'sellStock'
+    ]),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      this.sellStock()
+    }
+  }
+}
+</script>
